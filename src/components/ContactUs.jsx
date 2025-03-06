f10de293d97195bdaf0cc1e2acc9d4d9d7b8cd3e
Sagar Kotai <160866818+SagarKotai@users.noreply.github.com>
2025-03-06 22:43:56 +0530

    Update ContactUs.jsx

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { FaInstagram, FaLinkedin, FaTelegram } from "react-icons/fa";
+import { FaInstagram, FaLinkedin } from "react-icons/fa";
 import contactImg from '../assets/contact.jpg';
+import { SiGmail } from 'react-icons/si';
 
 const ContactUs = () => {
     return (
@@ -17,33 +18,35 @@ const ContactUs = () => {
                 </div>
                 <div className='m-10'>
                     <span className='text-4xl text-blue-500 hover:text-gray-400 m-5 font-bold'>Email</span>
-                    <p className='text-xl text-gray-500 hover:text-gray-400 m-5'>varunsharma@gmail.com</p>
+                    <p className='text-xl text-gray-500 hover:text-gray-400 m-5 '>stairsskill@gmail.com</p>
                 </div>
                 <div className='m-10'>
                     <span className='text-4xl text-blue-500 hover:text-gray-400 m-5 font-bold'>Fill out Google Form</span>
-                    <div className="my-5"><a className='text-xl text-gray-500 hover:text-gray-400 m-5' href="">Google Form</a></div>
+                    <div className="my-5"><a className='text-xl text-gray-500 hover:text-gray-400 m-5 underline' href="https://docs.google.com/forms/d/e/1FAIpQLSds4xtiL7DVU--rVjXkttTR4EnEVhd5MpUYW4b_GYnJoyxAwA/viewform?usp=header">Google Form</a></div>
                 </div>
                 <div className='flex justify-center text-3xl m-2 gap-6 my-5'>
                     <span>
-                        <a href="https://www.instagram.com/yourprofile" target="_blank" rel="noopener noreferrer">
+                        <a href="https://www.instagram.com/skillstairs.co" target="_blank" rel="noopener noreferrer">
                             <FaInstagram />
                         </a>
                     </span>
                     <span>
-                        <a href="https://www.linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer">
+                        <a href="http://www.linkedin.com/in/varunsharma0505" target="_blank" rel="noopener noreferrer">
                             <FaLinkedin />
                         </a>
                     </span>
                     <span>
-                        <a href="https://t.me/yourusername" target="_blank" rel="noopener noreferrer">
-                            <FaTelegram />
+                        <a href="mailto:stairsskill@gmail.com" target="_blank" rel="noopener noreferrer">
+                        <SiGmail />
                         </a>
                     </span>
                 </div>
                 <div className="text-center mt-10">
-                    <button className='px-14 py-5 shadow-2xl border-2 rounded-full text-2xl border-black hover:scale-105 hover:bg-gray-200 hover:border-blue-500 transition-transform'>
-                        Contact
-                    </button>
+                    <a href="mailto:stairsskill@gmail.com">
+                        <button className='px-14 py-5 shadow-2xl border-2 rounded-full text-2xl border-black hover:scale-105 hover:bg-gray-200 hover:border-blue-500 transition-transform'>
+                            Contact
+                        </button>
+                    </a>
                 </div>
             </div>
         </div>
